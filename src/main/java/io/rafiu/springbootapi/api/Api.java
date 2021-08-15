2aaae8c2c757cfc1bfcf6fbfecf5d552204dcaa9
Rafiu Jaman Mollah <rmollah@redhat.com>
2021-08-15 18:13:26 +0530

    first commit

---
+++ src/main/java/io/rafiu/springbootapi/api/Api.java
@@ -0,0 +1,14 @@
+package io.rafiu.springbootapi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Api.class, args);
+	}
+	
+}
---
+++ src/main/java/io/rafiu/springbootapi/api/TopicController.java
@@ -0,0 +1,28 @@
+package io.rafiu.springbootapi.api;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicService topicService;
+	
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/addTopic")
+	public void addtopic(@RequestBody Topic topic) {
+		
+		topicService.addTopic(topic);
+	}
+}
